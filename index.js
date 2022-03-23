08eaca579bef729047c69816df78b13b51c0986a
Bernardo Poetsch <89049723+nadupoetsch@users.noreply.github.com>
2022-03-23 02:52:04 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//'parseInt' é quando utilizamos números inteiros
+//para fazer calculos usamos o 'parseInt' para transformar o caractér em número
+// caso for usar números não inteiros, utilizar o 'parseFloat'
+
+var numeroA = parseInt(prompt('Digite o número A'))
+var numeroB = parseInt(prompt('Digite o número B'))
+
+var numeroC = numeroA + numeroB
+
+alert(numeroC)
